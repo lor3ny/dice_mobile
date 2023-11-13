027a843c8e6a117f4433d4936c3b5ae31c531535
Lorenzo Piarulli <lorenzo.piarulli3000@gmail.com>
2023-11-13 20:14:27 +0100

    Setting dice button features

--- Assets/DiceController.cs
+++ Assets/DiceController.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class DiceController : MonoBehaviour
+public class DiceController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
 
 
@@ -12,6 +14,29 @@ public class DiceController : MonoBehaviour
     
     //Public:
     public Sprite[] images;
+    private Button diceButton;
+
+
+    public void Initialize()
+    {
+        //diceButton.onClick.AddListener(empty);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        gameObject.GetComponent<Image>().color = Color.red;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        gameObject.GetComponent<Image>().color = new Color(1,1,1,1);
+    }
+
+
+    public void empty()
+    {
+        return;
+    }
 
     public void SetImage(int index)
     {
--- Assets/Lollo_Scripts/DiceManager.cs
+++ Assets/Lollo_Scripts/DiceManager.cs
@@ -7,29 +7,37 @@ public class DiceManager : MonoBehaviour
     //Private:
     private byte[] score;
     private byte heroesCount = 4;
-    private DiceController[] dices;
+    private DiceController[] dice;
 
     //Public:
     public GameObject dicePrefab;
     public Transform[] positions;
+
+    public DiceState currentState = DiceState.Null;
 
     private void Awake()
     {
 
         //heroesCount = LevelManager.GetHeroesCount();
 
-        dices = new DiceController[heroesCount];
+        dice = new DiceController[heroesCount];
 
         for (int i = 0; i < heroesCount; i++)
         {
             GameObject newDice = Instantiate(dicePrefab, positions[i].position, Quaternion.identity);
             newDice.transform.SetParent(this.transform);
-            dices[i] = newDice.GetComponent<DiceController>();
+            dice[i] = newDice.GetComponent<DiceController>();
+            dice[i].Initialize();
         }
+
+        currentState = DiceState.Unrolled;
     }
 
     public void Roll()
     {
+
+        if (currentState != DiceState.Unrolled)
+            return;
 
         //Random Generation
         score = new byte[heroesCount];
@@ -39,10 +47,22 @@ public class DiceManager : MonoBehaviour
             score[i] = (byte) Random.Range(0,7);    
         }
 
-
         //UI Animation
         UIRoll();
+
+        currentState = DiceState.Rolled;
     }
+
+
+    private void DiceSelection()
+    {
+        for(int i = 0;i < dice.Length; i++)
+        {
+            //dice[i].SetSelectable();
+        }
+    }
+
+
 
     private void UIRoll()
     {
@@ -50,8 +70,18 @@ public class DiceManager : MonoBehaviour
             return;
 
         for(int i = 0;i < score.Length;i++) {
-            dices[i].SetImage(score[i]);
+            dice[i].SetImage(score[i]);
             Debug.Log(score[i]);   
         }
     }
+
+    public enum DiceState
+    {
+        Null,
+        Unrolled,
+        Rolled,
+        Selected,
+    }
+
 }
+
